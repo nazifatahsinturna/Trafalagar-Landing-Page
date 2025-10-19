2aff276bb5fd331aa4c2bb759528f0e96a31c8b8
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-10-19 03:48:16 -0400

    tweeks added

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ ham.addEventListener('click', () => {
 cross.addEventListener('click', () => {
     topNav.classList.toggle('active');
 });
+// const heading = document.querySelector(".virtual h1");
+// function changeColor () {
+//     const blueColor = '#0000' + Math.floor(Math.random() * 256).toString(16).padStart(2, '0'); //generating color code
+//     heading.style.color = blueColor;
+    
+// }
+
+// //for automatic change
+// setInterval(changeColor, 500);
